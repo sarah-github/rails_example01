82935781bf656d1ecd0effc33ddcef2408981e75
sarah <sarah-github@stoffels.it>
2015-06-08 22:22:20 +0200

    changed login data handling to read it from secret file.

--- app/controllers/get_webservice_data_controller.rb
+++ app/controllers/get_webservice_data_controller.rb
@@ -5,7 +5,7 @@ class GetWebserviceDataController < ApplicationController
   
   def index
     client = Savon.client(wsdl: "https://www-genesis.destatis.de/genesisWS/services/DownloadService?wsdl")
-    response = client.call(:tabellen_download, message: {"kennung" => 'yourname', "passwort" => 'yourpass', "name" => '43111-0001', "bereich" => 'Alle', "format" => 'gsb', "komprimierung" => false, "startjahr" => '2015', "endjahr" => '2015', "zeitscheiben" => '', "regionalschluessel" => '', "sachmerkmal" => '', "sachschluessel" => '', "sprache" => ''})
+    response = client.call(:tabellen_download, message: {"kennung" => Rails.application.secrets.destatis_name, "passwort" => Rails.application.secrets.destatis_pw, "name" => '43111-0001', "bereich" => 'Alle', "format" => 'gsb', "komprimierung" => false, "startjahr" => '2015', "endjahr" => '2015', "zeitscheiben" => '', "regionalschluessel" => '', "sachmerkmal" => '', "sachschluessel" => '', "sprache" => ''})
 
     @xml = response.to_xml
   end
